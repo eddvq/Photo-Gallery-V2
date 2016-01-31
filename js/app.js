3729a3ce90b7662cd4e806e00a0c522abfa27a54
eddvq <eddyvzqz@gmail.com>
2016-01-31 02:17:37 -0800

    Validation Checked and X button Working

--- js/app.js
+++ js/app.js
@@ -3,27 +3,26 @@ var $overlay = $('<div id="overlay"></div>');
 var $image = $('<img class="show">');
 var $caption = $('<p class="caption"></p>');
 var $title = $('<h3 class="title"></h3>');
-var $leftArrow = $("<a href='#'><img src='images/buttons/arrow.png' id='buster'></a>").addClass('leftArrow');//here
+var $leftArrow = $("<a href='#'><img src='images/buttons/arrow.png'></a>").addClass('leftArrow');//here
 var $rightArrow = $("<a href='#'><img src='images/buttons/arrow.png'></a>").addClass('rightArrow');
 
+var $exit = $("<a href='#'><img src='images/buttons/exit.png' id='buster'></a>");
 
 var image;
 
-
 $("body").append($overlay);
-
-
 $overlay.append($leftArrow);
-
 $overlay.append($image);
 
 $overlay.append($rightArrow);
 
 $overlay.append($title);
 $overlay.append($caption);
+$overlay.append($exit);
 
 
-//Img Loader - Images need to be put into the empty space
+
+//Fills Page with Pics
 
 $(function(){
   //find a way to go through the image bank
@@ -34,29 +33,44 @@ $(function(){
   }
 });
 
-//Overlay Cancel 
-$overlay.on('click', function(){
-  $overlay.hide();
-});
-
-
-//Overlay Create + Show pic
+//Overlay Create + Show Gallery with Arros
 $('#gallery').on('click','li',function(){
     $overlay.show();
     image = $(this).children('img');
     imageLoader();
-    console.log(image);
+    //console.log(image);
     //Show larger image w src, alt, title 
 });
 
-//Image Loader
+
+//Overlay Cancel 
+$exit.on('click', function(){
+  $overlay.hide();
+});
+
+
+//Arrow Functionality 
+$('.leftArrow').on('click', function(event){
+  event.preventDefault();
+  image = image.parent().prev().children('img');
+  console.log(image);
+  imageLoader();
+});
+
+$('.rightArrow').on('click', function(event){
+  event.preventDefault();
+  image = image.parent().next().children('img');
+  imageLoader();
+});
+
+
+//Image Loader that puts pic, title and caption
 
 function imageLoader(){
     var imageLocate = image.attr('src').replace('/thumbnails/', '/');
     $image.attr('src', imageLocate);
     var imageTitle = image.attr('title');
     $title.text(imageTitle);
-    
     var imageCaption = image.attr('alt')
     $caption.text(imageCaption);
                       
